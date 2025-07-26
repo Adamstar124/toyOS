eb0b564bceff3d5f1ee59149801aba62289e333f
Adamstar124 <139605787+Adamstar124@users.noreply.github.com>
2025-07-26 23:31:08 +0800

    Update kernel.c
    add some comments.

--- kernel.c
+++ kernel.c
@@ -2,8 +2,8 @@ void main() {
     char* video = (char*) 0xb8000;
     const char* message = "Welcome to MyOS!";
     for (int i = 0; message[i] != '\0'; i++) {
-        video[i * 2] = message[i];
-        video[i * 2 + 1] = 0x0F;
+        video[i * 2] = message[i];    // 字元
+        video[i * 2 + 1] = 0x0F;      // 顏色：白底黑字
     }
 
     while (1);
